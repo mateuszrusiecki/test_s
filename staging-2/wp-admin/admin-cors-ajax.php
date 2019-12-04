08f3b036b3dd61ac505aea47bda2b50cbf877f00
MateuszR <mateusz.rusiecki@amsterdam-standard.pl>
2019-12-04 12:04:21 +0100

    remove unused code from admin-cors-ajax.php

--- staging-2/wp-admin/admin-cors-ajax.php
+++ staging-2/wp-admin/admin-cors-ajax.php
@@ -16,10 +16,6 @@ if ( 'OPTIONS' == $_SERVER['REQUEST_METHOD'] ) {
     die;
 }
 
-//echo json_encode($_POST);
-///var_dump($_REQUEST);
-//die( 'kkk' );
-
 /**
  * Executing Ajax process.
  *
@@ -34,12 +30,8 @@ if ( ! defined( 'WP_ADMIN' ) ) {
 require_once( dirname( dirname( __FILE__ ) ) . '/wp-load.php' );
 
 /** Allow for cross-domain requests (from the front end). */
-//send_origin_headers();
-//$origin = get_http_origin();
 header( 'Access-Control-Allow-Origin: *');
 header( 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE' );
-//header( 'Access-Control-Allow-Credentials: true' );
-//header( 'Vary: Origin' );
 
 
 // Require an action parameter
@@ -60,41 +52,6 @@ nocache_headers();
 
 /** This action is documented in wp-admin/admin.php */
 do_action( 'admin_init' );
-
-$core_actions_get = array(
-	'fetch-list', 'ajax-tag-search', 'wp-compression-test', 'imgedit-preview', 'oembed-cache',
-	'autocomplete-user', 'dashboard-widgets', 'logged-in',
-);
-
-$core_actions_post = array(
-	'oembed-cache', 'image-editor', 'delete-comment', 'delete-tag', 'delete-link',
-	'delete-meta', 'delete-post', 'trash-post', 'untrash-post', 'delete-page', 'dim-comment',
-	'add-link-category', 'add-tag', 'get-tagcloud', 'get-comments', 'replyto-comment',
-	'edit-comment', 'add-menu-item', 'add-meta', 'add-user', 'closed-postboxes',
-	'hidden-columns', 'update-welcome-panel', 'menu-get-metabox', 'wp-link-ajax',
-	'menu-locations-save', 'menu-quick-search', 'meta-box-order', 'get-permalink',
-	'sample-permalink', 'inline-save', 'inline-save-tax', 'find_posts', 'widgets-order',
-	'save-widget', 'delete-inactive-widgets', 'set-post-thumbnail', 'date_format', 'time_format',
-	'wp-remove-post-lock', 'dismiss-wp-pointer', 'upload-attachment', 'get-attachment',
-	'query-attachments', 'save-attachment', 'save-attachment-compat', 'send-link-to-editor',
-	'send-attachment-to-editor', 'save-attachment-order', 'heartbeat', 'get-revision-diffs',
-	'save-user-color-scheme', 'update-widget', 'query-themes', 'parse-embed', 'set-attachment-thumbnail',
-	'parse-media-shortcode', 'destroy-sessions', 'install-plugin', 'update-plugin', 'crop-image',
-	'generate-password', 'save-wporg-username', 'delete-plugin', 'search-plugins',
-	'search-install-plugins', 'activate-plugin', 'update-theme', 'delete-theme', 'install-theme',
-	'get-post-thumbnail-html', 'get-community-events', 'edit-theme-plugin-file',
-);
-
-// Deprecated
-$core_actions_post_deprecated = array( 'wp-fullscreen-save-post', 'press-this-save-post', 'press-this-add-category' );
-$core_actions_post = array_merge( $core_actions_post, $core_actions_post_deprecated );
-
-// Register core Ajax calls.
-if ( ! empty( $_GET['action'] ) && in_array( $_GET['action'], $core_actions_get ) )
-	add_action( 'wp_ajax_' . $_GET['action'], 'wp_ajax_' . str_replace( '-', '_', $_GET['action'] ), 1 );
-
-if ( ! empty( $_POST['action'] ) && in_array( $_POST['action'], $core_actions_post ) )
-	add_action( 'wp_ajax_' . $_POST['action'], 'wp_ajax_' . str_replace( '-', '_', $_POST['action'] ), 1 );
 
 add_action( 'wp_ajax_nopriv_heartbeat', 'wp_ajax_nopriv_heartbeat', 1 );
 
